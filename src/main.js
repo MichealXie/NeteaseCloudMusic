1f215a07b7e68b590f8f8d351f52ef12bc857d81
MichealXie <1441337380@qq.com>
2017-12-30 11:57:58 +0800

    引入 vuex

--- src/main.js
+++ src/main.js
@@ -4,12 +4,16 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import axios from 'axios'
+import {store} from './store/store'
+
+
 Vue.config.productionTip = false
 Vue.prototype.$http = axios
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store, 
   template: '<App/>',
   components: { App }
 })
