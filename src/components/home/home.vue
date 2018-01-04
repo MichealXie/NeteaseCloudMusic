067ea3a015215a4af885a81cd7391ed4d0c0774e
MichealXie <1441337380@qq.com>
2018-01-04 11:29:04 +0800

    增加 keep-alive

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -2,7 +2,9 @@
 	<div class="home">
 		<home-header></home-header>
     <home-tab></home-tab>
-		<router-view></router-view>
+		<keep-alive>
+			<router-view></router-view>
+		</keep-alive>
 	</div>
 </template>
 
