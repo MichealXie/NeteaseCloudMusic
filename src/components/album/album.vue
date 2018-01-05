828c45315dd63f19b12561096a11ebb7346f8955
MichealXie <1441337380@qq.com>
2018-01-05 09:57:10 +0800

    全新的专辑页~

---
+++ src/components/album/album.vue
@@ -0,0 +1,167 @@
+<template>
+	<div class="album">
+		<loading v-show="!albumSongs.length"></loading>
+		<div class="detail-header">
+			<div class="top">
+				<div class="icon" @click="goback()">
+					<i class="fa fa-chevron-left" aria-hidden="true"></i>
+				</div>
+				<p class="title">专辑</p>
+				<div class="icon">
+					<i aria-hidden="true" class="fa fa-headphones"></i>
+				</div>
+			</div>
+			<div class="detail-info">
+				<div class="img-ct">
+					<img :src="albumInfo.blurPicUrl">
+				</div>
+				<div class="list-creator">
+						<span class="name" >{{albumInfo.name}}</span>
+						<span class="singer">歌手: {{albumSongs[0].ar[0].name}}</span>
+				</div>
+			</div>
+			<div class="list-data">
+				<div class="icon-ct">
+					<i class="fa fa-plus-square-o" aria-hidden="true"></i>
+					<p>{{albumInfo.info.resourceId | playcount}}</p>
+				</div>
+				<div class="icon-ct">
+					<i class="fa fa-commenting-o" aria-hidden="true"></i>
+					<p>{{albumInfo.info.commentCount | playcount}}</p>
+				</div>
+				<div class="icon-ct">
+					<i class="fa fa-share-square-o" aria-hidden="true"></i>
+					<p>{{albumInfo.info.shareCount | playcount}}</p>					
+				</div>
+			</div>
+		</div>
+		<div class="detail-list">
+			<ul class="songs">
+				<li class="song" v-for="(item, index) in albumSongs" :key="item.id">
+					<span class="index">{{index + 1}}</span>
+					<div class="info">
+						<div class="name">{{item.name}}</div>
+						<div class="singer">{{item.ar[0].name}}</div>
+					</div>
+					<span class="placeholder"></span>
+				</li>
+			</ul>
+		</div>
+	</div>
+</template>
+
+<script>
+import loading from '@/base/loading/loading'
+
+export default {
+	components:{
+		loading,
+	},
+	methods: {
+		goback(){
+			history.back()
+		}
+	},
+	computed: {
+		albumSongs(){
+			return this.$store.state.albumSongs
+		},
+		albumInfo(){
+			return this.$store.state.albumInfo			
+		}
+	},
+	activated () {
+		this.$store.dispatch('getAlbum',this.$route.params.id)
+	}
+}
+</script>
+
+<style lang='stylus'>
+  @import "../../common/stylus/variable"
+  @import "../../common/stylus/mixin"
+
+	.album
+		.detail-header
+			color rgba(255,255,255,0.9)
+			background url('./background.png')
+			.top
+				display flex
+				align-items center
+				height 48px
+				width 100%
+				background url('./background.png') no-repeat
+				position fixed
+				z-index 2
+				.title
+					flex 1
+					text-align center
+				.icon
+					flex 0 0 24px
+					padding 10px
+			.detail-info
+				display flex
+				padding-top 48px
+				.img-ct
+					flex 1
+					position relative
+					margin 10px 0 0 20px
+					img 
+						width 100%
+					.count
+						position absolute
+						top 5px 
+						right 5px
+				.list-creator
+					flex 2
+					margin-left 20px
+					padding-top 25px
+					display flex
+					flex-direction column
+					justify-content center
+					.name
+						font-size 20px
+						padding 8px 0
+					.singer
+						default-singer()
+			.list-data
+				display flex
+				.icon-ct
+					flex 1
+					display flex
+					align-items center
+					flex-direction column
+					padding 8px 0
+					font-size $font-size-small				
+					i 
+						font-size $font-size-large-x
+						padding 8px 0
+		.detail-list
+			.songs
+				.song
+					display flex
+					no-wrap()
+					.index
+						flex 0 0 40px
+						height 50px
+						color $not-important
+						flex-center()
+					.info
+						flex 1
+						display flex
+						flex-direction column
+						justify-content center
+						width calc(100% - 80px)
+						border-1px()
+						.name
+							width 100%
+							song-name()
+							no-wrap()
+						.singer
+							width 100%
+							default-singer()
+							no-wrap()
+					.placeholder
+						flex 20px
+						border-1px()
+		
+</style>
