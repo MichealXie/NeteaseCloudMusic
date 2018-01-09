f9b3b09c38fadabea5ded6803a700a1e9f479ade
MichealXie <1441337380@qq.com>
2018-01-09 11:01:20 +0800

    完善, 实现基本功能

--- src/base/mini-player/mini-player.vue
+++ src/base/mini-player/mini-player.vue
@@ -1,11 +1,17 @@
 <template>
 	<div class="mini-player">
-		<div class="img-ct">
-			<img class="cover" src="http://p3.music.126.net/SpOw9nHtbBKnNhUYKevOaw==/7880199836658654.jpg">
+		<router-link to="/player" class="img-ct">
+			<img class="cover" :src="img">
+		</router-link>
+		<router-link to="/player" class="info">
+			<span class="song">{{song}}</span>
+			<span class="singer">{{singer}}</span>
+		</router-link>
+		<div class="play-btn" @click="togglePlay()">
+			<i class="fa" :class="{'fa-play-circle-o': !isPlay,'fa-pause-circle-o': isPlay}" aria-hidden="true"></i>
 		</div>
-		<div class="name"></div>
-		<div class="play-btn">
-			<i class="fa" :class="{'fa-play-circle-o': isPlay,'fa-pause-circle-o': !isPlay}" aria-hidden="true"></i>
+		<div class="next-btn" @click="nextSong()">
+			<i class="fa fa-step-forward" aria-hidden="true"></i>
 		</div>
 	</div>
 </template>
@@ -13,9 +19,50 @@
 <script>
 export default {
 	computed: {
+		img(){
+			if(this.playingList[this.currentSongIndex]) return this.playingList[this.currentSongIndex].al.picUrl
+			else{
+				return ''
+			}
+		},
+		song(){
+			if(this.playingList[this.currentSongIndex]) return this.playingList[this.currentSongIndex].name
+			else{
+				return "oops!"
+			}
+		},
+		singer(){
+			if(this.playingList[this.currentSongIndex]) return this.playingList[this.currentSongIndex].ar[0].name
+			else{
+				return "当前未播放~"
+			}
+		},
+		player(){
+			return document.getElementById('player')
+		},
 		isPlay(){
 			return this.$store.state.isPlay
-		}
+		},
+		currentSongIndex(){
+			return this.$store.state.currentSongIndex
+		},
+		playingList(){
+			return this.$store.state.playingList
+		},
+	},
+	methods: {
+		togglePlay(){
+			this.$store.commit('togglePlay')
+			if(this.isPlay) this.player.pause()
+			if(!this.isPlay) this.player.play()
+		},
+		nextSong(){
+			this.$store.commit('setIsPlay', false)			
+			this.$store.commit('songIndexAddOne')
+			let id = this.playingList[this.currentSongIndex].id
+			this.$store.dispatch('getSongUrl', id)
+			this.$store.commit('setIsPlay', true)
+		},
 	}
 }
 </script>
@@ -37,9 +84,17 @@ export default {
 			.cover
 				width 80%
 				height 80%
-		.name
+		.info
 			flex 1
-		.play-btn
+			display flex
+			flex-direction column
+			justify-content space-around
+			.song
+				song-name()
+				padding 0
+			.singer
+				default-singer()
+		.play-btn, .next-btn
 			flex 0 0 56px
 			font-size 30px
 			flex-center()
